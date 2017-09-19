111e035fd2128f33163fe0da3431e69d5fc73646
Anton Vukolov <vatnajokull89@gmail.com>
2017-09-19 23:52:20 +0300

    refactored add and delete List functions

--- src/app/lists/lists.controller.js
+++ src/app/lists/lists.controller.js
@@ -5,19 +5,27 @@
     .module('clientTodo')
     .controller('ListsController', function($rootScope, $scope, $state, $stateParams, List) {
 
-      var vm = this;
+      // var vm = this;
 
       // method to query the posts api and store the results in $scope
       // note: the linter will complain, but that can be fixed later:
       // You should not set properties on $scope in controllers. Use controllerAs syntax and add data to "this"
 
-      $scope.createNewList = function(newListName) {
-        new List({
-          name: newListName
-        }).create().then(value => {
-          list_query();
+      $scope.createList = function(listName) {
+        var list = new List({
+          name: listName
         });
-        $scope.newListName = '';
+        $scope.lists.push(list);
+        $scope.listName = '';
+        list.create();
+      };
+
+      $scope.removeList = function(list) {
+        var index = $scope.lists.indexOf(list);
+        if (index > -1) {
+          $scope.lists.splice(index, 1);
+        }
+        list.delete();
       };
 
       $scope.updateList = function(list, name) {
@@ -25,11 +33,11 @@
         $scope.editedList = null;
         list.update();
       };
-      // 
-      // $scope.editList = function(list) {
-      //   $scope.editedListName = list.name;
-      //   $scope.editedList = list;
-      // };
+
+      $scope.editList = function(list) {
+        $scope.editedListName = list.name;
+        $scope.editedList = list;
+      };
 
       $scope.cancelEdit = function(event) {
         if (event.which === 27) {
@@ -41,12 +49,6 @@
       var list_query = function(){
         List.query().then(function(lists){
           $scope.lists = lists;
-        });
-      };
-
-      $scope.removeList = function(list) {
-        list.delete().then(value => {
-          list_query();
         });
       };
 
