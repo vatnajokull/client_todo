3edc7c4250095a6820e5806cbf088e0297d84066
Anton Vukolov <vatnajokull89@gmail.com>
2017-10-03 21:03:49 +0300

    added ghostMode for browserSync

--- gulp/server.js
+++ gulp/server.js
@@ -4,33 +4,13 @@ require('dotenv').config();
 var path = require('path');
 var gulp = require('gulp');
 var conf = require('./conf');
-
 var browserSync = require('browser-sync');
 var browserSyncSpa = require('browser-sync-spa');
-
-var morgan = require('morgan');
-var express = require('express')
-var app = express()
 
 var util = require('util');
 
 var proxyMiddleware = require('http-proxy-middleware');
 
-function expressInit(baseDir) {
-  console.log('baseDir is ' + baseDir);
-  app.use(morgan('dev'));
-  app.use(express.static(__dirname + '/src'));
-
-  app.get('/', function (req, res) {
-    res.send('home')
-  })
-
-  app.middleware = proxyMiddleware('/api', {target: process.env.API_URL, changeOrigin: true});
-
-  app.listen(process.env.PORT, function () {
-    console.log('Example app listening on port ' + process.env.PORT)
-  })
-}
 
 function browserSyncInit(baseDir, browser) {
   browser = browser === undefined ? 'default' : browser;
@@ -53,7 +33,12 @@ function browserSyncInit(baseDir, browser) {
     startPath: '/',
     server: server,
     port: process.env.PORT,
-    browser: browser
+    browser: browser,
+    ghostMode: {
+        clicks: false,
+        forms: false,
+        scroll: false
+    }
   });
 }
 
